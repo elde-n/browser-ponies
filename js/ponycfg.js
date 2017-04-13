a9029f561b34562888af5355fe8112b5333b4d30
Marcelo Seidel Jr <jackiedevpe@outlook.com.br>
2017-04-13 02:20:47 -0300

    Update ponycfg.js

--- js/ponycfg.js
+++ js/ponycfg.js
@@ -1,6 +1,6 @@
 var BrowserPoniesConfig = {
 	autostart: true,
-	spawn: {}, spawnRandom: 2,
+	spawn: {"derpy hooves": 1}, spawnRandom: 1,
 	interactions: pony_bp_interactions,
 	ponies: pony_bp_ponies
 };
